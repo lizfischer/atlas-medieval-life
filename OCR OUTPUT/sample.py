49da77f39ba7672a7a10f5ac92b21624e50f7498
Liz Fischer <lizzy.m.fischer@gmail.com>
2022-04-11 16:08:15 -0500

    gis batches

--- OCR OUTPUT/sample.py
+++ OCR OUTPUT/sample.py
@@ -2,20 +2,44 @@ import os, shutil, random
 
 full_pool = os.listdir("ALL DOCUMENTS TXT")  # handlist + hcm + hcm2 + fix
 small_sample = os.listdir("GIS Tag Test small")
+large_sample = os.listdir("GIS Tag Test large")
 
-already_used = small_sample
+already_used = small_sample + large_sample
 
 
-while True:
-    large_sample = random.sample(full_pool, 100)
-    duplicated = [s for s in large_sample if s in small_sample]
-    print(len(duplicated))
-    if len(duplicated) == 0:
-        break
-print(len(large_sample))
+def random_sample():
+    while True:
+        new_sample = random.sample(full_pool, 100)
+        duplicated = [s for s in new_sample if s in small_sample]
+        print(len(duplicated))
+        if len(duplicated) == 0:
+            break
+    print(len(new_sample))
 
-for f in large_sample:
-   shutil.copyfile(os.path.join("ALL DOCUMENTS TXT", f), os.path.join("GIS Tag Test large", f))
+    for f in new_sample:
+       shutil.copyfile(os.path.join("ALL DOCUMENTS TXT", f), os.path.join("GIS rest", f))
+
+def get_rest(already_used):
+    for f in full_pool:
+        if f not in already_used:
+            shutil.copyfile(os.path.join("ALL DOCUMENTS TXT", f), os.path.join("GIS rest", f))
+            print(f)
+
+import math
+def make_batches(files, batch_size):
+    n_files = len(files)
+    n_batches = math.ceil(n_files / batch_size)
+    for i in range(0, n_batches):
+        start = i * batch_size
+        batch_files = files[start:start+batch_size]
+        os.mkdir(f"GIS rest/{i+1}")
+        for f in batch_files:
+            shutil.move(os.path.join("GIS rest", f), os.path.join(f"GIS rest/{i+1}", f))
+        
+
+
+
+make_batches(os.listdir("GIS rest"), 10)
 
 def check_numbers():
     ## HCM "UNCORRECTED"

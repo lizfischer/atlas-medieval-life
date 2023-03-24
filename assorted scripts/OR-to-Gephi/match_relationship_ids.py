de6b1bdc7059c95cb3ef115982464f4c995534fb
Liz Fischer <lizzy.m.fischer@gmail.com>
2023-03-24 13:34:54 -0500

    .

---
+++ assorted scripts/OR-to-Gephi/match_relationship_ids.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+people_df = pd.read_csv('All-People.csv')
+rel_df = pd.read_csv('All-Relationships.csv')
+rel_df = rel_df[rel_df['source_id'].notna()]
+
+
+def translate_ids(row, field):
+    match = people_df.loc[people_df['UUID'].str.contains(row[field], na=False), "Person ID"]
+    return None if len(match) == 0 else match.iloc[0]
+
+
+rel_df['source_person_id'] = rel_df.apply(lambda row: translate_ids(row, 'source_id'), axis=1)
+rel_df['target_person_id'] = rel_df.apply(lambda row: translate_ids(row, 'target_id'), axis=1)
+rel_df.to_csv("All-Relationships_modified.csv")

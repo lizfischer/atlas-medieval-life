2245047796e3efcaa11e785cdf877043c692f109
Liz Fischer <lizzy.m.fischer@gmail.com>
2024-02-12 22:42:53 -0800

    Populating DB

---
+++ database/adding_documents.py
@@ -0,0 +1,103 @@
+from csv import DictReader
+from tqdm import tqdm
+import sqlite3
+
+connection = sqlite3.connect('atlas_database_jan_2024.db')
+cursor = connection.cursor()
+
+
+# cursor.execute('''INSERT INTO documents (type, source, source_number) VALUES ("account", "HCM", "2368")''')
+# connection.commit()
+# connection.close()
+
+def parse_doc_info(input_file="doc_info.csv"):
+    with open(input_file, "r", encoding="utf8") as f:
+        reader = DictReader(f)
+        doc_list = list(reader)
+    docs = []
+
+    for d in doc_list:
+        d['Earliest Date'] = d['Earliest Date'].split('T')[0].strip()
+        d['Latest Date'] = d['Latest Date'].split('T')[0].strip()
+        d['type'] = d['General Doc Type'].strip().lower()
+        if "HCM" in d['Alternate Numbering']:
+            d['source_num'] = d['Alternate Numbering'].split('HCM ')[1]
+            d['source'] = "HCM"
+        else:
+            d['source'] = None
+            d['source_num'] = None
+        if "RBLR" in d['Number']:
+            d['rblr'] = d['Number'].split(" ")[1]
+        else:
+            d['rblr'] = None
+        docs.append(d)
+    return docs
+
+
+def insert_date(doc_info, commit=False):
+    query = '''INSERT INTO dates(earliest_date, latest_date) VALUES(?, ?)'''
+    cursor.execute(query, (doc_info['Earliest Date'], doc_info['Latest Date'],))
+    if commit:
+        connection.commit()
+    return cursor.lastrowid
+
+
+def lookup_date(doc_info):
+    query = '''SELECT * FROM dates 
+            WHERE earliest_date = ?
+            AND latest_date = ?'''
+
+    cursor.execute(query, (doc_info['Earliest Date'], doc_info['Latest Date'],))
+    result = cursor.fetchall()
+    return result
+
+
+def _lookup_doc(doc_info):
+    query = '''SELECT * FROM documents 
+            WHERE source = ?
+            AND source_number = ?
+            AND handlist_number = ?'''
+
+    cursor.execute(query, (doc_info['source'], doc_info['source_num'], doc_info['rblr'],))
+    result = cursor.fetchall()
+    return result
+
+
+def insert_docs(input_file="doc_info.csv"):
+    query = '''INSERT INTO documents(type, source, source_number, handlist_number, date, date_text, date_notes) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)'''
+
+    docs = parse_doc_info()
+    for d in tqdm(docs):
+        if _lookup_doc(d):
+            continue
+        # cursor.execute("INSERT INTO documents(type, source, source_number, handlist_number", ())
+        date = lookup_date(d)
+        if not date:
+            date_id = insert_date(d)
+        else:
+            date_id = date[0][0]
+
+        cursor.execute(query, (d['type'], d['source'], d['source_num'], d['rblr'], date_id, d['DATE'], d['Date notes'],))
+        connection.commit()
+
+
+def parse_doc_types(input_file="doc_types.csv"):
+    with open(input_file, "r") as f:
+        lines = f.readlines()
+    types = []
+    for t in lines:
+        types.append(t.strip().lower())
+
+    return types
+
+
+def insert_doc_types(input_file="doc_types.csv"):
+    types = parse_doc_types(input_file)
+    for t in types:
+        print(t)
+        cursor.execute("INSERT INTO document_types (doc_type) VALUES (?)", (t,))
+    connection.commit()
+
+
+connection.close()
---
+++ database/adding_people.py
@@ -0,0 +1,102 @@
+from csv import DictReader
+from tqdm import tqdm
+import sqlite3
+
+connection = sqlite3.connect('atlas_database_jan_2024.db')
+cursor = connection.cursor()
+
+
+def _parse_all_people(input_file):
+    with open(input_file, "r", encoding="utf8") as f:
+        reader = DictReader(f)
+        person_list = list(reader)
+
+    people = []
+    for p in person_list:
+        people.append(p)
+    return people
+
+
+def _lookup_person(person_info):
+    query = '''SELECT * FROM people 
+                WHERE person_id = ?'''
+    cursor.execute(query, (person_info['Person ID'],))
+    result = cursor.fetchall()
+    return result
+
+
+def get_doc_id_by_sourcenum(source: str, num: str) -> list:
+    if source.upper() == "RBLR":
+        query = '''SELECT document_id FROM documents 
+               WHERE handlist_number = ?'''
+        cursor.execute(query, (num,))
+    else:
+        query = '''SELECT * FROM documents 
+                   WHERE source = ?
+                   AND source_number = ?'''
+        cursor.execute(query, (source, num,))
+    result = cursor.fetchone()
+    if not result:
+        raise Exception("Document number not found")
+    return result[0]
+
+
+def _lookup_person_in_doc(person_id, doc_id):
+    query = '''SELECT relationship_id FROM person_named_in_document 
+                  WHERE person_id = ?
+                  AND document_id = ?'''
+    cursor.execute(query, (person_id, doc_id,))
+    result = cursor.fetchone()
+    return result
+
+
+def _insert_person_in_doc(p, doc_id):
+    if _lookup_person_in_doc(p['Person ID'], doc_id):
+        return
+    query = '''INSERT INTO person_named_in_document(document_id, person_id, person_name_transcription, dead, named_by)
+                VALUES(?, ?, ?, ?, ?)'''
+    cursor.execute(query, (doc_id, p['Person ID'], p['QUOTE_TRANSCRIPTION'], int(bool(p['Dead'])), p['Named by']))
+    connection.commit()
+
+
+def _lookup_person_role(rel_id, role):
+    query = '''SELECT * FROM person_document_roles WHERE relationship_id = ? AND role = ?'''
+    cursor.execute(query, (rel_id, role,))
+    result = cursor.fetchone()
+    return result
+
+
+def _insert_person_roles(p, doc_id):
+    rel_id = _lookup_person_in_doc(p['Person ID'], doc_id)[0]
+
+    query = '''INSERT INTO person_document_roles(relationship_id, role) VALUES (?, ?)'''
+    if p['Principal'] and not _lookup_person_role(rel_id, p['Principal']):
+        cursor.execute(query, (rel_id, "Principal"))
+    if p['Scribe'] and not _lookup_person_role(rel_id, p['Scribe']):
+        cursor.execute(query, (rel_id, "Scribe"))
+    if p['Witness'] and not _lookup_person_role(rel_id, p['Witness']):
+        cursor.execute(query, (rel_id, "Witness"))
+
+
+def insert_people(input_file="all_people.csv"):
+    query = '''INSERT INTO people(person_id, display_name, gender, clerical) VALUES (?,?,?,?)'''
+    people = _parse_all_people(input_file)
+    for p in tqdm(people):
+        if not _lookup_person(p):
+            # Add person
+            if not p['Gender']:
+                p['Gender'] = "U"
+            cursor.execute(query, (p['Person ID'], p['Normalized text'], p['Gender'], int(bool(p['Clerical?'])),))
+
+        source, num = p['Doc Number'].split(" ")
+        try:
+            doc_id = get_doc_id_by_sourcenum(source, num)
+            _insert_person_in_doc(p, doc_id)
+            _insert_person_roles(p, doc_id)
+        except Exception as e:
+            with open("errors.txt", "a") as error_file:
+                error_file.write(f"Error: {e} on {p}\n")
+        connection.commit()
+
+
+insert_people()
